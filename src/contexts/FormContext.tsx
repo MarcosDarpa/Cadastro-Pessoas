7320905dcc99378e0dc102eade56810f1aedef92
Marcos Lopes <130925272+MarcosDarpa@users.noreply.github.com>
2024-07-17 13:07:36 -0300

    FormContext Provider

--- src/contexts/FormContext.tsx
+++ src/contexts/FormContext.tsx
@@ -1,6 +1,13 @@
 // Context, Reducer, Provider, Hook
 import { createContext, useContext, useReducer } from "react";
 
+const initialData = {
+    currentStep: 0,
+    name: '',
+    level: 0,
+    email: '',
+    github: ''
+}
 // Context
 const FormContext = createContext(undefined);
 
@@ -13,19 +20,30 @@ enum FormActions {
     setGithub
 }
 const FormReducer = (state, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case FormActions.setCurrentStep:
-            return {...state, currentStep: action.payload};
+            return { ...state, currentStep: action.payload };
         case FormActions.setName:
-            return {...state, name: action.payload};
+            return { ...state, name: action.payload };
         case FormActions.setLevel:
-            return {...state, level: action.payload};
+            return { ...state, level: action.payload };
         case FormActions.setEMail:
-            return {...state, email: action.paylound};
+            return { ...state, email: action.paylound };
         case FormActions.setGithub:
-            return {...state, Github: action.payloand};
-            default:
-                return state;
+            return { ...state, Github: action.payloand };
+        default:
+            return state;
     }
 
+}
+
+//Provider
+const FormProvider = ({ childrean }) => {
+    const [state, dispatch] = useReducer(FormReducer, initialData);
+    const value = { state, dispatch };
+    return (
+        <FormContext.Provider value={value}>
+            {childrean}
+        </FormContext.Provider>
+    )
 }
