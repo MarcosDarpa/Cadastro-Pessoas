64e5f9d27d97479d1cdb574eaa6b0cef97c0a344
Marcos Lopes <130925272+MarcosDarpa@users.noreply.github.com>
2024-07-23 23:40:53 -0300

    Adicionada validação de nome e exibição de mensagem de erro no FormStep1

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,28 +1,56 @@
-
+import { useNavigate } from 'react-router-dom';
 import * as C from './styles';
+import { useForm, FormActions } from '../../contexts/FormContext';
 import { Theme } from '../../components/Theme';
-
-const handleNextStep = () => {
-
-}
+import { ChangeEvent, useEffect } from 'react';
 
 export const FormStep1 = () => {
+    const navigate = useNavigate();
+    const { state, dispatch } = useForm();
+
+    useEffect(() => {
+        dispatch({
+            type: FormActions.setCurrentStep,
+            payload: 1
+        });
+    }, [dispatch]);
+
+    const handleNextStep = () => {
+        if(state.name !== '') {
+            navigate('/step2');
+        }else{
+            alert("Preencha os dados.");
+        }
+        
+    }
+
+    const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        dispatch({
+            type: FormActions.setName,
+            payload: e.target.value
+        });
+    }
+
     return (
         <Theme>
             <C.Container>
-                <p>Passo 1/3</p>
-                <h1>Vamos começar com seu nome: </h1>
+                <p>Passo 1/3 {state.name}</p>
+                <h1>Vamos começar com seu nome:</h1>
                 <p>Preencha o campo abaixo com seu nome completo.</p>
 
                 <hr />
 
                 <label>
                     Seu nome completo
-                    <input type="text" autoFocus 
+                    <input
+                        type="text"
+                        autoFocus
+                        value={state.name}
+                        onChange={handleNameChange}
                     />
                 </label>
 
-                <button onClick={ handleNextStep }>Próximo</button>
+                <button onClick={handleNextStep}>Próximo</button>
             </C.Container>
         </Theme>
     );
